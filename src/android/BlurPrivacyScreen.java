c1fafc45629e4f3ad05a930c90fa09a233c1c594
LlamaloX <sergioazpiazu@gmail.com>
2021-02-12 12:32:38 -0500

    Create BlurPrivacyScreen.java

---
+++ src/android/BlurPrivacyScreen.java
@@ -0,0 +1,40 @@
+/**
+ * BlurPrivacyScreen.java
+ * Created by s3rious a.k.a Leonid Semenov on 09/09/2019
+ * Copyright (c) 2019 s3rious a.k.a Leonid Semenov. All rights reserved.
+ * MIT Licensed
+ */
+package com.llamalox.blurprivacyscreen;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaActivity;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
+
+import android.app.Activity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.os.Bundle;
+
+public class BlurAppPrivacyScreen extends CordovaActivity {
+
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    // Initialize plugin
+    super.initialize(cordova, webView);
+  
+  }
+ 
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+   getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+   super.onCreate(savedInstanceState);
+  }
+}
