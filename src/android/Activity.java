6d94744c581f5859acae3742a6c58ea70c4246e2
LlamaloX <sergioazpiazu@gmail.com>
2021-02-12 14:05:39 -0500

    Update Activity.java

--- src/android/Activity.java
+++ src/android/Activity.java
@@ -26,6 +26,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 public class Activity{
+  @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
    getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
    super.onCreate(savedInstanceState);
